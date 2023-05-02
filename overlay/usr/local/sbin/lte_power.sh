08543c8ae567c9ef81b66e552dade7c022649a9e
Josua Mayer <josua@solid-run.com>
2023-05-02 12:09:32 +0300

    linux: document and simplify lte module power-on

--- overlay/usr/local/sbin/lte_power.sh
+++ overlay/usr/local/sbin/lte_power.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-GPIO_VBAT=14 # 0*32+14
 GPIO_RESETN=43 # 1*32+11
 GPIO_PWRKEY=44 # 1*32+12
 
-for gpio in $GPIO_VBAT $GPIO_RESETN $GPIO_PWRKEY; do
+for gpio in $GPIO_RESETN $GPIO_PWRKEY; do
     if [ ! -e /sys/class/gpio/gpio$gpio ]; then
         echo $gpio > /sys/class/gpio/export
     fi
     echo out > /sys/class/gpio/gpio$gpio/direction
 done
 
-cat /sys/class/gpio/gpio$GPIO_VBAT/value
 cat /sys/class/gpio/gpio$GPIO_RESETN/value
 cat /sys/class/gpio/gpio$GPIO_PWRKEY/value
 
@@ -19,7 +17,7 @@ cat /sys/class/gpio/gpio$GPIO_PWRKEY/value
 # - disable vbat
 # - assert reset
 # - deassert power key
-echo 0 > /sys/class/gpio/gpio$GPIO_VBAT/value
+echo disabled > /sys/devices/platform/userspace-consumer-lte-vbat/state
 echo 0 > /sys/class/gpio/gpio$GPIO_RESETN/value
 echo 0 > /sys/class/gpio/gpio$GPIO_PWRKEY/value
 
@@ -29,7 +27,7 @@ sleep 1
 # - enable vbat
 # - deassert reset
 # - toggle power key
-echo 1 > /sys/class/gpio/gpio$GPIO_VBAT/value
+echo enabled > /sys/devices/platform/userspace-consumer-lte-vbat/state
 echo 1 > /sys/class/gpio/gpio$GPIO_RESETN/value
 echo 1 > /sys/class/gpio/gpio$GPIO_PWRKEY/value
 echo 0 > /sys/class/gpio/gpio$GPIO_PWRKEY/value
